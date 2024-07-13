7a55bd6f43c5e0933a451a1590f90d4c89639b0b
inspiringfirdy <95465813+inspiringfirdy@users.noreply.github.com>
2024-07-14 01:22:49 +0800

    Update resignation_app.py

--- resignation_app.py
+++ resignation_app.py
@@ -41,8 +41,8 @@ def adjust_holidays(holidays, off_days, rest_days):
 # Input parameters from the user
 notice_received_date = st.date_input("Date of Manager Acknowledgement", datetime(2024, 7, 15))
 notice_period_months = st.number_input("Notice Period (Months)", value=1, min_value=0)
-requested_last_working_day = st.date_input("Requested Last Working Day", datetime(2024, 8, 2))
-leave_balance = st.number_input("Leave Balance (Days)", value=20, min_value=0)
+requested_last_working_day = st.date_input("Requested Last Working Day", datetime(2024, 8, 14))
+leave_balance = st.number_input("Leave Balance (Days)", value=0, min_value=0)
 off_day = st.selectbox("Off Day", ["Saturday", "Sunday"], index=0)
 rest_day = st.selectbox("Rest Day", ["Saturday", "Sunday"], index=1)
 
@@ -82,6 +82,9 @@ try:
     updated_leave_balance = leave_balance - leave_used_to_offset_notice
     short_notice_days = days_unserved - leave_used_to_offset_notice
 
+    # Set unserved notice dates to "N/A" if there are no unserved days
+    unserved_notice_dates_str = "N/A" if days_unserved == 0 else f"{unserved_notice_start.strftime('%d/%m/%Y')} - {unserved_notice_end.strftime('%d/%m/%Y')}"
+
     # Option handling based on leave balance
     option_1_leave_dates = []
     if leave_balance > 0:
@@ -92,10 +95,10 @@ try:
                 option_1_leave_dates.append(current_date)
             current_date -= timedelta(days=1)
         option_1_leave_dates = option_1_leave_dates[::-1]  # Reverse to start from last working day backward
-        last_physical_date_option_1 = option_1_leave_dates[0] - timedelta(days=1) if option_1_leave_dates else None
+        last_physical_date_option_1 = requested_last_working_day if leave_balance == 0 else option_1_leave_dates[0] - timedelta(days=1)
         last_payroll_date_option_1 = requested_last_working_day
     else:
-        last_physical_date_option_1 = requested_last_working_day - timedelta(days=1)
+        last_physical_date_option_1 = requested_last_working_day
         last_payroll_date_option_1 = requested_last_working_day
 
     option_2_extended_dates = []
@@ -118,7 +121,7 @@ try:
         "Notice Served (Date)": f"{notice_served_start.strftime('%d/%m/%Y')} - {notice_served_end.strftime('%d/%m/%Y')}",
         "Total Number of Days Served": days_served,
         "Full Notice Period (Days)": full_notice_period_days,
-        "Unserved Notice (Date)": f"{unserved_notice_start.strftime('%d/%m/%Y')} - {unserved_notice_end.strftime('%d/%m/%Y')}",
+        "Unserved Notice (Date)": unserved_notice_dates_str,
         "Total Number of Days Unserved": days_unserved,
         "Total Leave Balance": leave_balance,
         "Leave Used to Offset Notice": leave_used_to_offset_notice,
