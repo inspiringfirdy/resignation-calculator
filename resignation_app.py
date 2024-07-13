b6fc9b0787d3f960bc7f974a04e325cd228d3793
inspiringfirdy <95465813+inspiringfirdy@users.noreply.github.com>
2024-07-13 16:15:15 +0800

    Update resignation_app.py

--- resignation_app.py
+++ resignation_app.py
@@ -162,12 +162,12 @@ Resignation Details:
 - Last Working Day Requested: {last_physical_working_day.strftime('%d/%m/%Y')}
 
 Leave and Payroll Details:
-- LEAVE BALANCE: {unused_leave_days} days
-- Number of Leave Days Used to Offset Short Notice: {leave_used_to_offset_short_notice}
-- Number of Leave Days Used to be Cleared During Workdays Throughout Notice Period: {leave_to_clear_during_notice}
-- Number of Leave Days Used to Extend the Last PHYSICAL Working Date: {leave_used_to_extend}
-- Last Payroll Date (Salary paid up to): {last_payroll_date.strftime('%d/%m/%Y')}
-{unserved_notice_info}
+
+LEAVE BALANCE: {unused_leave_days} days
+Number of Leave Days Used to Offset Short Notice: {leave_used_to_offset_short_notice}
+Number of Leave Days Used to be Cleared During Workdays Throughout Notice Period: {leave_to_clear_during_notice}
+Number of Leave Days Used to Extend the Last PHYSICAL Working Date: {leave_used_to_extend}
+Last Payroll Date (Salary paid up to): {last_payroll_date.strftime('%d/%m/%Y')}
 
 You are required to ensure the clearances/actions below are fulfilled to ensure a smooth process:
 
